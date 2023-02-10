f91b3929d27027174aa2edc0ab947f84c51a6c49
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-02-10 15:16:05 +0300

    time_delta field added, agg func - count added for counting amount of books

--- list_books/models.py
+++ list_books/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.shortcuts import reverse
 from pytils.translit import slugify
@@ -10,6 +12,7 @@ class Books(models.Model):
     author = models.CharField(max_length=100)
     date_start = models.DateTimeField(null=True)
     date_end = models.DateTimeField(null=True)
+    time_delta = models.IntegerField(null=True)
     slug = models.SlugField(default='', null=False)
 
     def get_url(self):
@@ -17,4 +20,9 @@ class Books(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = f'{slugify(self.title)}-{slugify(self.author)}'
+        if self.time_delta is None:
+            if self.date_start and self.date_end:
+                self.time_delta = (self.date_end - self.date_start).days
+            elif self.date_start and not self.date_end:
+                self.time_delta = (datetime.datetime.now() - self.date_start.replace(tzinfo=None)).days
         super().save(*args, **kwargs)
--- list_books/views.py
+++ list_books/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
 from .models import Books
 # Create your views here.
 
 
 def get_list_books(request):
     books = Books.objects.all()
+    total = books.aggregate(Count('id'))
     for book in books:
         book.save()
     data = {
-        'books': books
+        'books': books,
+        'total': total
     }
     return render(request, 'list_books/list_books.html', context=data)
 
